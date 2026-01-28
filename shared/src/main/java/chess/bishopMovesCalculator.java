841a6c48ba90068464ca5f288fc78d1670eb025c
Clayton Stallings <crstall2004@gmail.com>
2026-01-27 19:25:13 -0700

    got the first test to not just throw an exception, but instead have wrong moves.

--- shared/src/main/java/chess/bishopMovesCalculator.java
+++ shared/src/main/java/chess/bishopMovesCalculator.java
@@ -9,6 +9,7 @@ public class bishopMovesCalculator implements moveCalculator {
     @Override
     public Collection<ChessMove> possibleMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> moves = new ArrayList<>();
-        moves.add()
+        moves.add(new ChessMove(myPosition, new ChessPosition(1,1), null));
+        return moves;
     }
 }
