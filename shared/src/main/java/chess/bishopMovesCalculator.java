e1bb44d030aace7a64d2d65e79939e92bf375c58
Clayton Stallings <crstall2004@gmail.com>
2026-01-27 19:19:32 -0700

    Starting to create the move calculators

---
+++ shared/src/main/java/chess/bishopMovesCalculator.java
@@ -0,0 +1,14 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class bishopMovesCalculator implements moveCalculator {
+
+    @Override
+    public Collection<ChessMove> possibleMoves(ChessBoard board, ChessPosition myPosition) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+        moves.add()
+    }
+}
--- shared/src/main/java/chess/moveCalculator.java
+++ shared/src/main/java/chess/moveCalculator.java
@@ -1,4 +1,7 @@
 package chess;
 
+import java.util.Collection;
+
 public interface moveCalculator {
+    Collection<ChessMove> possibleMoves(ChessBoard board, ChessPosition myPosition);
 }
