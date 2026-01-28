9934a161426fd25228733f1848585e146959e331
Clayton Stallings <crstall2004@gmail.com>
2026-01-27 19:45:21 -0700

    Correctly calculated bishop moves (i think)

--- shared/src/main/java/chess/bishopMovesCalculator.java
+++ shared/src/main/java/chess/bishopMovesCalculator.java
@@ -6,10 +6,41 @@ import java.util.List;
 
 public class bishopMovesCalculator implements moveCalculator {
 
+    private void calculate(ChessBoard board, ChessPosition myPosition, ArrayList<ChessMove> output, ChessGame.TeamColor myTeam, int myRow, int myCol, int rowdir, int coldir){
+        int testRow = myRow;
+        int testCol = myCol;
+        testRow+=rowdir;
+        testCol+=coldir;
+        boolean taken = false;
+        while(testRow <= 8 && testRow >= 1 && testCol <= 8 && testCol >= 1 && !taken){
+            ChessPosition testPos = new ChessPosition(testRow,testCol);
+            if(board.getPiece(testPos) == null){
+                output.add(new ChessMove(myPosition, testPos, null));
+                testRow+=rowdir;
+                testCol+=coldir;
+            }
+            else{
+                if(board.getPiece(testPos).getTeamColor() != myTeam){
+                    output.add(new ChessMove(myPosition, testPos, null));
+                }
+                taken = true;
+            }
+        }
+
+    }
+
     @Override
     public Collection<ChessMove> possibleMoves(ChessBoard board, ChessPosition myPosition) {
+        int myRow = myPosition.getRow();
+        int myCol = myPosition.getColumn();
+        ChessGame.TeamColor myTeam = board.getPiece(myPosition).getTeamColor();
+
         ArrayList<ChessMove> moves = new ArrayList<>();
-        moves.add(new ChessMove(myPosition, new ChessPosition(1,1), null));
+        calculate(board,myPosition,moves,myTeam,myRow,myCol,1,1);
+        calculate(board,myPosition,moves,myTeam,myRow,myCol,-1,1);
+        calculate(board,myPosition,moves,myTeam,myRow,myCol,1,-1);
+        calculate(board,myPosition,moves,myTeam,myRow,myCol,-1,-1);
+
         return moves;
     }
 }
